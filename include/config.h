d25105dbe37fdedff77c19e60e53d73b5c76cb9c
jionfull <jionfull@jionfull.cn>
2011-12-12 20:03:16 +0800

    原始代码

---
+++ include/config.h
@@ -0,0 +1,7 @@
+#define CONFIG_SYS_USE_DATAFLASH 1
+
+/* Automatically generated - do not edit */
+#define CONFIG_BOARDDIR board/atmel/at91sam9263ek
+#include <config_defaults.h>
+#include <configs/at91sam9263ek.h>
+#include <asm/config.h>
---
+++ include/config.mk
@@ -0,0 +1,5 @@
+ARCH   = arm
+CPU    = arm926ejs
+BOARD  = at91sam9263ek
+VENDOR = atmel
+SOC    = at91
